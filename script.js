760e36c8fdbc76f78be4ad2063af274edcb6a96e
Alejo Ribes <alejoribes@gmail.com>
2015-06-03 11:37:36 +0200

    Style Movile Added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 arrayPilots = [];
 $(document).ready(function(){
-	getPilot("Rossi");
+	getPilot("Marquez");
 	getPilot("Lorenzo");
 	function getPilot(namePilot){
 		$.ajax({
@@ -15,30 +15,33 @@ $(document).ready(function(){
 
 	function drawPilot(pilot){
         arrayPilots.push(pilot);
-        console.log(pilot.last_name);
-		$("#pilots").append("<span id='" + pilot.last_name + "'></span>")
-        $("#" + pilot.last_name+"").text(" - " + pilot.last_name);
+		$("#pilots").append("<div id='" + pilot.last_name + "'></div>")
+        $("#" + pilot.last_name+"").text("Last name: - " + pilot.last_name);
 
-        $("#pilots").append("<span id='" + pilot.number + "'></span>")
-        $("#" + pilot.number+"").text(" - " + pilot.number);
+        $("#pilots").append("<div id='" + pilot.number + "'></div>")
+        $("#" + pilot.number+"").text("Number:  - " + pilot.number);
 
-        $("#pilots").append("<span id='" + pilot.n_races + "'></span>")
-        $("#" + pilot.n_races+"").text(" - " + pilot.n_races);
+        $("#pilots").append("<div id='" + pilot.n_championships + "'></div>")
+        $("#" + pilot.n_championships + "").text("Championships: - " + pilot.n_championships );
 
-        $("#pilots").append("<span id='" + pilot.n_victory + "'></span>")
-        $("#" + pilot.n_victory + "").text(" - " + pilot.n_victory );
+        $("#pilots").append("<div id='" + pilot.n_races + "'></div>")
+        $("#" + pilot.n_races+"").text("Number of Races: - " + pilot.n_races);
 
-        $("#pilots").append("<span id='" + pilot.n_second_position + "'></span>")
-        $("#" + pilot.n_second_position + "").text(" - " + pilot.n_second_position );
+        $("#pilots").append("<div id='" + pilot.n_victory + "'></div>")
+        $("#" + pilot.n_victory + "").text("Victories: - " + pilot.n_victory );
 
-       $("#pilots").append("<span id='" + pilot.n_third_position + "'></span>")
-        $("#" + pilot.n_third_position + "").text(" - " + pilot.n_third_position );
+        $("#pilots").append("<div id='" + pilot.n_second_position + "'></div>")
+        $("#" + pilot.n_second_position + "").text("Second position: - " + pilot.n_second_position );
 
-        $("#pilots").append("<span id='" + pilot.n_pole_positions + "'></span>")
-        $("#" + pilot.n_pole_positions + "").text(" - " + pilot.n_pole_positions );
+       $("#pilots").append("<div id='" + pilot.n_third_position + "'></div>")
+        $("#" + pilot.n_third_position + "").text("Third Position: - " + pilot.n_third_position );
 
-        $("#pilots").append("<span id='" + pilot.n_fastest_lap + "'></span>")
-        $("#" + pilot.n_fastest_lap + "").text(" - " + pilot.n_fastest_lap );		
+        $("#pilots").append("<div id='" + pilot.n_pole_positions + "'></div>")
+        $("#" + pilot.n_pole_positions + "").text("Pole Position: - " + pilot.n_pole_positions );
+
+        $("#pilots").append("<div id='" + pilot.n_fastest_lap + "'></div>")
+        $("#" + pilot.n_fastest_lap + "").text("Fast Lap: - " + pilot.n_fastest_lap );
+        $("#pilots").append("<img src='" + pilot.url_photo + "'>")	
 	};
 });
 //end
@@ -61,7 +64,7 @@ $("#viewGraphic").on("click",function(){
                 'Second',
                 'Third',
                 'Pole',
-                'Fast Lap'
+                'Fast Lap',
             ],
             crosshair: true
         },
