530bf9d9f6282e0b2ba96277db748578dc2a06b0
Alejo Ribes <alejoribes@gmail.com>
2015-06-09 11:04:39 +0200

    Graphics attendance and all victories added

--- test/testMap.js
+++ test/testMap.js
@@ -13,6 +13,8 @@ $(document).ready(function(){
 		var allLatitudes = getLatitudes(gp_races);
 		var allLongitudes = getLongitudes(gp_races);
 		var allNames = getNames(gp_races);
+		drawGraphicAttendance(gp_races);
+		drawVictoriesGraphic(gp_races);
 		var map = new GMaps({
       		el: '#map',
       		lat: 29.799583,
@@ -52,19 +54,118 @@ $(document).ready(function(){
 		}
 		return allNames;
 	};
-
-	/*var arrCoordLat = [-12.043333, -11.043333, -10.043333];
-	var arrCoodLong = [-77.028333,-76.028333, -75.028333];
-	 var map = new GMaps({
-      el: '#map',
-      lat: -12.043333,
-      lng: -77.028333
+	function drawGraphicAttendance(gp_races){
+		   var chart = new Highcharts.Chart({
+        chart: {
+            renderTo: 'graphicAttendance',
+            type: 'column',
+            margin: 75,
+            options3d: {
+                enabled: true,
+                alpha: 55,
+                beta: 55,
+                depth: 20,
+                viewDistance: 25
+            }
+        },
+        title: {
+            text: 'Attendance in each GP'
+        },
+        subtitle: {
+            text: 'Season 2014'
+        },
+        plotOptions: {
+            column: {
+                depth: 25
+            }
+        },
+        series: [{
+            data: [gp_races[0].attendance_2014, gp_races[1].attendance_2014, gp_races[2].attendance_2014, gp_races[3].attendance_2014, gp_races[4].attendance_2014, gp_races[5].attendance_2014, gp_races[6].attendance_2014, gp_races[7].attendance_2014, gp_races[8].attendance_2014, gp_races[9].attendance_2014, gp_races[10].attendance_2014, gp_races[11].attendance_2014, gp_races[12].attendance_2014, gp_races[13].attendance_2014, gp_races[14].attendance_2014, gp_races[15].attendance_2014, gp_races[16].attendance_2014, gp_races[17].attendance_2014]
+        }]
     });
-	for(var i=0; i <= arrCoodLong.length-1; i++){
-		 map.addMarker({
-  		lat: arrCoordLat[i],
-  		lng: arrCoodLong[i],
-  		title: 'Bitch',
-		});
-	};*/
+
+	    function showValues() {
+	        $('#R0-value').html(chart.options.chart.options3d.alpha);
+	        $('#R1-value').html(chart.options.chart.options3d.beta);
+	    }
+
+    	// Activate the sliders
+	    $('#R0').on('change', function () {
+	        chart.options.chart.options3d.alpha = this.value;
+	        showValues();
+	        chart.redraw(false);
+	    });
+	    $('#R1').on('change', function () {
+	        chart.options.chart.options3d.beta = this.value;
+	        showValues();
+	        chart.redraw(false);
+	    });
+
+	    showValues();
+		}
+	function drawVictoriesGraphic(gp_races){
+			var allVictories = countVictories(gp_races);
+			 $('#graphicVictories').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: 0,
+            plotShadow: false
+        },
+        title: {
+            text: '% Victories',
+            align: 'center',
+            verticalAlign: 'middle',
+            y: 50
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                dataLabels: {
+                    enabled: true,
+                    distance: -50,
+                    style: {
+                        fontWeight: 'bold',
+                        color: 'white',
+                        textShadow: '0px 1px 2px black'
+                    }
+                },
+                startAngle: -90,
+                endAngle: 90,
+                center: ['50%', '75%']
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Browser share',
+            innerSize: '50%',
+            data: [
+                ['Marquez',   allVictories[0]],
+                ['Lorenzo',       allVictories[1]],
+                ['Rossi', 		allVictories[2]],
+                ['Pedrosa',    allVictories[3]],
+                
+            ]
+        }]
+    });
+	};
+	function countVictories(gp_races){
+		var arrayVictories = [0,0,0,0];
+		for(var i=0; i <= gp_races.length-1; i++){
+			if(gp_races[i].winner_2014 === 'Marquez'){
+				arrayVictories[0] += 1;
+			}
+			else if(gp_races[i].winner_2014 === 'Lorenzo'){
+				arrayVictories[1] +=1;
+			}
+			else if(gp_races[i].winner_2014 === 'Rossi'){
+				arrayVictories[2] += 1;
+			}
+			else if(gp_races[i].winner_2014 === 'Pedrosa'){
+				arrayVictories[3] += 1;
+			}
+		};
+		return arrayVictories;
+	};
 });
